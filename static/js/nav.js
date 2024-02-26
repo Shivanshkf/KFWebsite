35f27a10422fe35f45255c735a644056391d2e9d
Kirana Friends <shivansh.m@kiranafriends.com>
2024-02-26 19:44:52 +0530

    fixes uses

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,12 +1,24 @@
 var menuIcon = document.getElementById("menu-icon");
+// function openNav() {
+//   let e = document.getElementById("sidenav");
+//   "100%" == e.style.width
+//     ? (menuIcon.classList.toggle("open"), (e.style.width = "0px"))
+//     : (e.style.width = "100%");
+// }
+// document.addEventListener("DOMContentLoaded", function () {
+//   menuIcon.addEventListener("click", function () {
+//     this.classList.toggle("open");
+//   });
+// });
+
 function openNav() {
   let e = document.getElementById("sidenav");
-  "100%" == e.style.width
-    ? (menuIcon.classList.toggle("open"), (e.style.width = "0px"))
-    : (e.style.width = "100%");
+  if (e.style.width == "100%") {
+      // This means the navigation is currently open, so we're going to close it.
+      e.style.width = "0px"; // Close the navigation
+  } else {
+      e.style.width = "100%"; // Open the navigation
+  }
+  // Regardless of the condition above, toggle the 'open' class each time the function is called.
+  menuIcon.classList.toggle("open");
 }
-document.addEventListener("DOMContentLoaded", function () {
-  menuIcon.addEventListener("click", function () {
-    this.classList.toggle("open");
-  });
-});
