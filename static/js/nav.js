4f07b060b338dc54ccf077545133552b43f76e74
Kirana Friends <shivansh.m@kiranafriends.com>
2024-02-28 12:50:21 +0530

    fixes uses

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,16 +1,4 @@
 var menuIcon = document.getElementById("menu-icon");
-// function openNav() {
-//   let e = document.getElementById("sidenav");
-//   "100%" == e.style.width
-//     ? (menuIcon.classList.toggle("open"), (e.style.width = "0px"))
-//     : (e.style.width = "100%");
-// }
-// document.addEventListener("DOMContentLoaded", function () {
-//   menuIcon.addEventListener("click", function () {
-//     this.classList.toggle("open");
-//   });
-// });
-
 function openNav() {
   let e = document.getElementById("sidenav");
   if (e.style.width == "100%") {
